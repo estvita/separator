5aafeb57066cbe132ac8b79d42b817f06061a7fa
gulin <antgulin@ya.ru>
2025-07-29 07:57:31 +0500

    asgi upd

--- config/asgi.py
+++ config/asgi.py
@@ -2,12 +2,14 @@
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-import thoth.asterx.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+django_asgi_app = get_asgi_application()
+
+import thoth.asterx.routing
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": URLRouter(
         thoth.asterx.routing.websocket_urlpatterns
     ),
