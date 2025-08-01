7a2f546e5c366f92d931dca82d50fcf541749507
gulin <antgulin@ya.ru>
2025-08-01 20:09:50 +0500

    refresh_token upd

--- thoth/asterx/api/views.py
+++ thoth/asterx/api/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import action
 from django.utils import timezone
 from thoth.asterx.models import Server
 from thoth.bitrix.crest import refresh_token
-from thoth.bitrix.models import Bitrix, Credential
+from thoth.bitrix.models import Bitrix
 
 class AsterxHandler(GenericViewSet):
     permission_classes = [IsAuthenticated]
@@ -14,8 +14,7 @@ class AsterxHandler(GenericViewSet):
     def refresh_token(self, request):
         server_id = request.data.get('server_id')
         member_id = request.data.get('member_id')
-        access_token = request.data.get('access_token')
-        if not server_id or not member_id or not access_token:
+        if not server_id or not member_id:
             return Response({"error": "ID is required."}, status=400)
         try:
             server = Server.objects.get(id=server_id)
@@ -27,17 +26,17 @@ class AsterxHandler(GenericViewSet):
             return Response({"error": "Server license has expired."}, status=402)
         
         try:
-            Bitrix.objects.get(member_id=member_id)
+            portal = Bitrix.objects.get(member_id=member_id)
         except Bitrix.DoesNotExist:
             return Response({"error": "B24 not found."}, status=403)
         
-        try:
-            credential = Credential.objects.get(access_token=access_token)
-        except Credential.DoesNotExist:
-            return Response({"error": "Credential not found."}, status=403)
-        
-        if refresh_token(credential):
-            credential.refresh_from_db()
-            return Response({"access_token": credential.access_token})
+        active_users = portal.users.filter(active=True)
+        for user in active_users:
+            credential = user.credentials.filter(app_instance=server.settings.app_instance).first()
+            if not credential:
+                continue        
+            if refresh_token(credential):
+                credential.refresh_from_db()
+                return Response({"access_token": credential.access_token})
 
         return Response({"status": "ok"})
