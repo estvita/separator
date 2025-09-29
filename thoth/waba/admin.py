81827c12f05a45c3add6640d5c279b3a3b2060c3
gulin <antgulin@ya.ru>
2025-09-29 16:51:22 +0500

    instance filter

--- thoth/waba/admin.py
+++ thoth/waba/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin, messages
-from django.db import transaction
+from django.db import transaction, models
+from django import forms
 import thoth.bitrix.utils as bitrix_utils
+from thoth.bitrix.models import AppInstance, Line
 
 from .models import App, Waba, Phone, Template
 from .tasks import call_management
@@ -20,8 +22,39 @@ class TemplateAdmin(admin.ModelAdmin):
     list_per_page = 30
 
 
+class SessionForm(forms.ModelForm):
+    class Meta:
+        model = Phone
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        owner = self.instance.owner or self.initial.get('owner')
+
+        app_instance_qs = AppInstance.objects.all()
+        line_qs = Line.objects.all()
+
+        if owner:
+            app_instance_qs = app_instance_qs.filter(owner=owner)
+            line_qs = line_qs.filter(owner=owner)
+
+        if self.instance.pk:
+            if self.instance.app_instance:
+                app_instance_qs = AppInstance.objects.filter(
+                    models.Q(pk=self.instance.app_instance.pk) | models.Q(owner=owner)
+                )
+            if self.instance.line:
+                line_qs = Line.objects.filter(
+                    models.Q(pk=self.instance.line.pk) | models.Q(owner=owner)
+                )
+
+        self.fields['app_instance'].queryset = app_instance_qs.distinct()
+        self.fields['line'].queryset = line_qs.distinct()
+
+
 @admin.register(Phone)
 class PhoneAdmin(admin.ModelAdmin):
+    form = SessionForm
     list_display = ("phone_id", "phone", "owner", "date_end", "sip_extensions", "sms_service")
     search_fields = ("phone", "phone_id")
     list_filter = ("calling", )
--- thoth/waweb/admin.py
+++ thoth/waweb/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.db import transaction
+from django.db import transaction, models
+from django.contrib import messages
+from django import forms
 from .models import Session, Server
 from thoth.bitrix.models import Connector, AppInstance, Line
 import thoth.bitrix.utils as bitrix_utils
-from django.contrib import messages
-from django import forms
 
 
 class SessionForm(forms.ModelForm):
@@ -15,9 +15,27 @@ class SessionForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         owner = self.instance.owner or self.initial.get('owner')
+
+        app_instance_qs = AppInstance.objects.all()
+        line_qs = Line.objects.all()
+
         if owner:
-            self.fields['app_instance'].queryset = AppInstance.objects.filter(owner=owner)
-            self.fields['line'].queryset = Line.objects.filter(owner=owner)
+            app_instance_qs = app_instance_qs.filter(owner=owner)
+            line_qs = line_qs.filter(owner=owner)
+
+        if self.instance.pk:
+            if self.instance.app_instance:
+                app_instance_qs = AppInstance.objects.filter(
+                    models.Q(pk=self.instance.app_instance.pk) | models.Q(owner=owner)
+                )
+            if self.instance.line:
+                line_qs = Line.objects.filter(
+                    models.Q(pk=self.instance.line.pk) | models.Q(owner=owner)
+                )
+
+        self.fields['app_instance'].queryset = app_instance_qs.distinct()
+        self.fields['line'].queryset = line_qs.distinct()
+
 
 @admin.register(Session)
 class SessionAdmin(admin.ModelAdmin):
