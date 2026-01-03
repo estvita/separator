c7133d10c4295964fa2e2f7de4a334bfc3d7d767
gulin <antgulin@ya.ru>
2026-01-03 17:24:01 +0500

    ext check_status

--- separator/freepbx/api/views.py
+++ separator/freepbx/api/views.py
@@ -10,6 +10,9 @@ import separator.waba.tasks as waba_tasks
 class ExtViewSet(GenericViewSet):
     @action(detail=False, methods=['get'])
     def check_status(self, request):
+        if not request.user.is_staff:
+            return Response({"error": "permission denied"}, status=403)
+
         phone = request.GET.get('phone')
         if not phone:
             return Response({"error": "phone is required."}, status=400)
@@ -21,11 +24,11 @@ class ExtViewSet(GenericViewSet):
             return Response({"error": "disabled"}, status=404)
         ext = phone_obj.sip_extensions
         if ext.date_end and timezone.now() > ext.date_end:
-            disabled = True
-        if disabled:
-            phone_obj.calling = "disabled"
-            phone_obj.call_dest = "disabled"
-            phone_obj.save()
-            transaction.on_commit(lambda: waba_tasks.call_management.delay(phone_obj.id))
             return Response({"error": "payment required"}, status=402)
+        #     disabled = True
+        # if disabled:
+        #     phone_obj.calling = "disabled"
+        #     phone_obj.call_dest = "disabled"
+        #     phone_obj.save()
+        #     transaction.on_commit(lambda: waba_tasks.call_management.delay(phone_obj.id))
         return Response("success")
