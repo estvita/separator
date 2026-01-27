00ab739dfa954f0785f0bfa3188f40d07c38c9ae
gulin <antgulin@ya.ru>
2026-01-27 20:32:54 +0500

    Add file attachment support to WAWeb send_message_view

--- separator/waweb/forms.py
+++ separator/waweb/forms.py
@@ -8,5 +8,20 @@ class SendMessageForm(forms.Form):
     )
     message = forms.CharField(
         widget=forms.Textarea(attrs={"placeholder": _("Enter message text")}),
-        label=None
+        label=None,
+        required=False
     )
+    file = forms.FileField(
+        label=_("Attachment"),
+        required=False
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        message = cleaned_data.get("message")
+        file = cleaned_data.get("file")
+
+        if not message and not file:
+            raise forms.ValidationError(_("You must provide either a message or a file."))
+        
+        return cleaned_data
